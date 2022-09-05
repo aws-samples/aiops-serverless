14e3cf0319cd0c79f644da4b235e6361344c6291
Chetan Makvana <makvac@amazon.co.uk>
2022-09-05 13:50:39 +0100

    Added application frontend and backend files

---
+++ application/resources/js/awsconfig.js
@@ -0,0 +1,7 @@
+const DEBUG = true;
+const AWS_CONFIG = {
+    "region" : "<region where the env is deployed>",
+    "API_ENDPOINT" : "<API Gateway invoke URL>/v1/",
+    "APPNAME" : "<name of the application>"
+};
+exports.AWS_CONFIG = AWS_CONFIG;
